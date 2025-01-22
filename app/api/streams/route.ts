2c05b990e9ec78244912093018b22a61f2411c37
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2025-01-22 19:42:54 +0530

    regex

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,22 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import { prismaClient } from "@/app/lib/db";
+const YT_REGEX = new RegExp(
+  "^https://www.youtube.com/watch?v=[w-]+(&[w-]+=?[^s]*)*$"
+);
 
 const CreateStreamSchema = z.object({
   creatorId: z.string(),
-  url: z
-    .string()
-    .refine(
-      (url) => url.includes("youtube.com") || url.includes("spotify.com"),
-      {
-        message: "URL must be from YouTube or Spotify",
-      }
-    ),
+  url: z.string(),
 });
 
 export async function Post(req: NextRequest) {
   try {
     const data = CreateStreamSchema.parse(await req.json());
+    prismaClient.stream.create({
+      userId: data.creatorId,
+      url: data.url,
+    });
   } catch (error) {
     return NextResponse.json(
       {
